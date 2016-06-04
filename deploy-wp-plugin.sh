3cd67fc391eb538921a4704491aa589b0f880780
KamataRyo <mugil.cephalus+github@gmail.com>
2016-06-04 23:09:55 +0900

    Test

--- deploy-wp-plugin.sh
+++ deploy-wp-plugin.sh
@@ -3,25 +3,35 @@
 # [name] Deploy WP Plugin
 # [description] Deploy a WordPress Plugin to Github and svn Plugin from Travis CI
 
+# [Environmental Variables]
+# - $WP_VERSION_TO_DEPLOY
+# - $PHP_VERSION_TO_DEPLOY
+# - $WP_MULTISITE_TO_DEPLOY
+# - $GH_REF
+# - $SVN_REF
+# - $GH_TOKEN
+# - $SVN_USER
+# - $SVN_PASS
+
 set -e
 
 # filter whether deploy or not
 if ! [[ "$WP_VERSION"         == "$WP_VERSION_TO_DEPLOY" && \
-	    "$TRAVIS_PHP_VERSION" == "$PHP_VERSION_TO_DEPLOY" && \
-	    "$WP_MULTISITE"       == "$WP_MULTISITE_TO_DEPLOY" ]]; then
-	echo "Not deploying from this matrix.";
-	exit 0
+        "$TRAVIS_PHP_VERSION" == "$PHP_VERSION_TO_DEPLOY" && \
+        "$WP_MULTISITE"       == "$WP_MULTISITE_TO_DEPLOY" ]]; then
+    echo "Not deploying from this matrix.";
+    exit 0
 elif [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
-	echo "Not deploying from pull requests."
-	exit 0
+    echo "Not deploying from pull requests."
+    exit 0
 elif ! [[ "master" == "$TRAVIS_BRANCH" ]]; then
-	echo "Not on the 'master' branch."
-	if [[ "" == "$TRAVIS_TAG" ]]; then
-		echo "Not tagged."
-		exit 0
-	else
-		echo "tagged."
-	fi
+    echo "Not on the 'master' branch."
+    if [[ "" == "$TRAVIS_TAG" ]]; then
+        echo "Not tagged."
+        exit 0
+    else
+        echo "tagged."
+    fi
 fi
 
 # store commit message for later process
@@ -30,7 +40,7 @@ COMMIT_MESSAGE=$(git log --format=%B -n 1 "$TRAVIS_COMMIT")
 # rebuild and format repo for release
 rm -rf .git
 if [[ -e ".svnignore" ]]; then
-	cat .svnignore > .gitignore
+    cat .svnignore > .gitignore
 fi
 
 git init
@@ -41,14 +51,16 @@ git commit --quiet -m "Deploy from travis." -m "Original commit is $TRAVIS_COMMI
 
 # github release on 'latest' branch
 if [[ "master" == "$TRAVIS_BRANCH" ]]; then
-	echo "enforcing pushing to 'latest'.."
-	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:latest > /dev/null 2>&1
-	echo "deployed on 'latest' branch, which is tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
+    echo "enforcing pushing to 'latest'.."
+    echo "$GH_REF" # test
+    echo "$SVN_USER" # test
+    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:latest > /dev/null 2>&1
+    echo "deployed on 'latest' branch, which is tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
 fi
 
 if [[  "" == "$TRAVIS_TAG" ]]; then
-	echo "Not deploying without tag."
-	exit 0
+    echo "Not deploying without tag."
+    exit 0
 fi
 
 # github tagged release.
@@ -61,13 +73,13 @@ git push --force --quiet --tag "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
 echo "deployed as '$TRAVIS_TAG', tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
 
 if [[ "" != $SVN_USER || "" !=$SVN_PASS ]]; then
-	echo 'not committing to svn.'
-	exit 0
+    echo 'not committing to svn.'
+    exit 0
 fi
 
 if [[ "-d tags/${TRAVIS_TAG}" ]]; then
-	echo "'tags/${TRAVIS_TAG}' already exists."
-	exit 0
+    echo "'tags/${TRAVIS_TAG}' already exists."
+    exit 0
 fi
 
 #svn release
@@ -76,16 +88,17 @@ GIT_ROOT=$(pwd)
 SVN_ROOT="~/svn"
 mkdir "$SVN_ROOT"
 cd "$SVN_ROOT"
-svn co "$SVN_REPO"
+svn co "$SVN_REF"
 
 echo 'Updating svn repo..'
 rm -rf trunk/*
 cp "${GIT_REPO}/*" trunk/
 if [[ -e ".svnignore" ]]; then
-	svn propset -R svn:ignore -F .svnignore .
+    svn propset -R svn:ignore -F .svnignore .
 fi
 svn cp trunk "tags/${TRAVIS_TAG}"
 svn add *
-svn ci -q -m "Deploy from travis." -m "Original commit is ${TRAVIS_COMMIT}." > /dev/null 2>&1
+svn ci -q -m "Deploy from travis. Original commit is ${TRAVIS_COMMIT}." \
+--username "$SVN_USER" --password "$SVN_PASS"  > /dev/null 2>&1
 
 exit 0
