8de47b086257d2809f589b76b62cc76a33270991
KamataRyo <mugil.cephalus+github@gmail.com>
2016-06-04 22:24:53 +0900

    Add case WordPress plugins

---
+++ deploy-wp-plugin.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# [name] Deploy WP Plugin
+# [description] Deploy a WordPress Plugin to Github and svn Plugin from Travis CI
+
+set -e
+
+# filter whether deploy or not
+if ! [[ "$WP_VERSION"         == "$WP_VERSION_TO_DEPLOY" && \
+	    "$TRAVIS_PHP_VERSION" == "$PHP_VERSION_TO_DEPLOY" && \
+	    "$WP_MULTISITE"       == "$WP_MULTISITE_TO_DEPLOY" ]]; then
+	echo "Not deploying from this matrix.";
+	exit 0
+elif [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+	echo "Not deploying from pull requests."
+	exit 0
+elif ! [[ "master" == "$TRAVIS_BRANCH" ]]; then
+	echo "Not on the 'master' branch."
+	if [[ "" == "$TRAVIS_TAG" ]]; then
+		echo "Not tagged."
+		exit 0
+	else
+		echo "tagged."
+	fi
+fi
+
+# store commit message for later process
+COMMIT_MESSAGE=$(git log --format=%B -n 1 "$TRAVIS_COMMIT")
+
+# rebuild and format repo for release
+rm -rf .git
+if [[ -e ".svnignore" ]]; then
+	cat .svnignore > .gitignore
+fi
+
+git init
+git config user.name "kamataryo"
+git config user.email "kamataryo@travis-ci.org"
+git add .
+git commit --quiet -m "Deploy from travis." -m "Original commit is $TRAVIS_COMMIT."
+
+# github release on 'latest' branch
+if [[ "master" == "$TRAVIS_BRANCH" ]]; then
+	echo "enforcing pushing to 'latest'.."
+	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:latest > /dev/null 2>&1
+	echo "deployed on 'latest' branch, which is tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
+fi
+
+if [[  "" == "$TRAVIS_TAG" ]]; then
+	echo "Not deploying without tag."
+	exit 0
+fi
+
+# github tagged release.
+echo "Deleting remote not compiled tag '$TRAVIS_TAG'.."
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" ":$TRAVIS_TAG" > /dev/null 2>&1
+echo "Making new tag with compiled files..."
+git tag "$TRAVIS_TAG" -m "$COMMIT_MESSAGE" -m "Original commit is $TRAVIS_COMMIT."
+echo "Pushing new tag '$TRAVIS_TAG'..."
+git push --force --quiet --tag "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
+echo "deployed as '$TRAVIS_TAG', tested on PHP=$TRAVIS_PHP_VERSION & WP=$WP_VERSION"
+
+if [[ "" != $SVN_USER || "" !=$SVN_PASS ]]; then
+	echo 'not committing to svn.'
+	exit 0
+fi
+
+if [[ "-d tags/${TRAVIS_TAG}" ]]; then
+	echo "'tags/${TRAVIS_TAG}' already exists."
+	exit 0
+fi
+
+#svn release
+echo 'preparing svn repo..'
+GIT_ROOT=$(pwd)
+SVN_ROOT="~/svn"
+mkdir "$SVN_ROOT"
+cd "$SVN_ROOT"
+svn co "$SVN_REPO"
+
+echo 'Updating svn repo..'
+rm -rf trunk/*
+cp "${GIT_REPO}/*" trunk/
+if [[ -e ".svnignore" ]]; then
+	svn propset -R svn:ignore -F .svnignore .
+fi
+svn cp trunk "tags/${TRAVIS_TAG}"
+svn add *
+svn ci -q -m "Deploy from travis." -m "Original commit is ${TRAVIS_COMMIT}." > /dev/null 2>&1
+
+exit 0
---
+++ travis-encrypt-wp.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+# [name] Travis Encrypt WP
+# [description] Encrypt GH_TOKEN, SVN_USER, SVN_PASS at once
+# [usage] travis-encrypt-wp {GH_TOKEN} {SVN_USER}
+# [dependency] Run on Mac. It uses keychain to find
+
+
+service="<https://plugins.svn.wordpress.org:443> Use your WordPress.org login"
+GH_TOKEN=$1
+SVN_USER=$2
+SVN_PASS=`security find-generic-password -wgs "$service" -a$SVN_USER`
+
+travis encrypt GH_TOKEN="$GH_TOKEN" SVN_USER="$SVN_USR" SVN_PASS="$SVN_PASS"
