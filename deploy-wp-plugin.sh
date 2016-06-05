ffd5c32369daed610e7bbcc1a04a7d0f4ead66cf
KamataRyo <mugil.cephalus+github@gmail.com>
2016-06-05 12:56:10 +0900

    Fix

--- deploy-wp-plugin.sh
+++ deploy-wp-plugin.sh
@@ -101,7 +101,7 @@ if [[ -e "${SVN_ROOT}/trunk/.svnignore" ]]; then
 fi
 
 echo "Commiting to svn.."
-ls -R "${SVN_ROOT}"
+ls "${SVN_ROOT}"
 svn add "${SVN_ROOT}"
 svn ci -q -m "Deploy from travis. Original commit is ${TRAVIS_COMMIT}." \
 --username "$SVN_USER" --password "$SVN_PASS"  > /dev/null 2>&1
